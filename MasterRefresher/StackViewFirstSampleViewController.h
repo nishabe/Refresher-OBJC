fd14bf0bfc6f7d4656148e2f5d6bbeeeae4c47a8
nishabe <aneeshabraham1@gmail.com>
2016-11-04 15:31:53 +0530

    Added sample for stack view

---
+++ MasterRefresher/StackViewFirstSampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  StackViewFirstSampleViewController.h
+//  Refresher-OBJC
+//
+//  Created by on 11/4/16.
+//  Copyright © 2016 Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StackViewFirstSampleViewController : UIViewController
+
+@end
---
+++ MasterRefresher/StackViewFirstSampleViewController.m
@@ -0,0 +1,24 @@
+//
+//  StackViewFirstSampleViewController.m
+//  Refresher-OBJC
+//
+//  Created by  on 11/4/16.
+//  Copyright © 2016  Inc. All rights reserved.
+// Ref: https://www.appcoda.com/stack-views-intro/
+
+#import "StackViewFirstSampleViewController.h"
+
+@interface StackViewFirstSampleViewController ()
+
+@end
+
+@implementation StackViewFirstSampleViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+     // Do any additional setup after loading the view.
+    // Visit the link above for detailed instructions.
+}
+@end
